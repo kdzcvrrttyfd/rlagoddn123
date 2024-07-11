1f1a329fb390a8f97e34ec17792cddbc48867f00
desk <qudwndh@gmail.com>
2024-07-11 14:58:06 +0900

    2

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('FLASK_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
